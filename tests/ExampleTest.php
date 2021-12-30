727c632e51b6d560966441377415cb7d2b12be0e
m-a-t-t-h <m-a-t-t-h@users.noreply.github.com>
2021-12-30 10:52:44 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,7 +5,8 @@ namespace Matt\Weight\Tests;
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function it_can_test() {
+    public function it_can_test()
+    {
         $pounds = \Matt\Weight\WeightClass::kilograms(100)->toPounds();
         $this->assertEquals($pounds, 220.46);
     }
